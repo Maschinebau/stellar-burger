2e610d8aeeb876114db56bcc0858428af5cdf8d8
Osipov Alexander <saxmail0722@gmail.com>
2023-07-23 11:55:40 +0500

    webpack prefs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./scss/styles.scss";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const title = 'React с Webpack и Babel';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>{title}</h1>); 
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
